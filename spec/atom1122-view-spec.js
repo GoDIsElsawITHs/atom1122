4b0ff9db252215a840dcb5ef286aa820eb58c429
sawith <sawith2541@gmail.com>
2022-09-19 12:56:30 +0700

    atom1122

---
+++ spec/atom1122-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Atom1122View from '../lib/atom1122-view';
+
+describe('Atom1122View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
